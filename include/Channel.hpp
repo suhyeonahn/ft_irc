70f469058142fa2a00f9e06aa49302cc908362a9
Suhyeon AHN <suahn@e2r12p5.clusters.42paris.fr>
2022-10-24 18:44:39 +0200

    Channel has only _i and _k modes and operator prefix

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -7,7 +7,7 @@
 
 # define CHAN_PREFIX '#'
 # define CHAN_INVALID_CHAR "\a,: "
-# define CHANNEL_MODE   "beliIkmstn"
+# define OPER_PREFIX '@'
 
 class   Channel
 {
@@ -23,17 +23,8 @@ class   Channel
         set<User *>     _invitedList; // Invited Users in Channel
 
         //  Channel mode
-        bool    _b; //  Ban Channel Mode
-        bool    _e; //  Exception Channel Mode
-        bool    _l; //  Client Limit Channel Mode
         bool    _i; //  Invite-Only Channel Mode
-        bool    _I; //  Invite-Exception Channel Mode
         bool    _k; //  Key Channel Mode
-        bool    _m; //  Moderated Channel Mode
-        bool    _s; //  Secret Channel Mode
-        bool    _t; //  Protected Topic Mode
-        bool    _n; //  No External Messages Mod
-
 
         void    setMode( bool plus, char const & mode );
 
@@ -41,7 +32,6 @@ class   Channel
         void    rmUser( User * user );
 
 		void	sendMsg( int code, string params[], vector<t_ClientMsg> & res );
-
 
 	public:
 
@@ -54,6 +44,7 @@ class   Channel
         string  getNicks() const;
         size_t  getNusers() const;
         string  getMode()  const;
+        User *  getOperByNick( string const & nick ) const;
 
 		static bool IsPrefix(char c);
 		static bool IsValidName(const string &name);
